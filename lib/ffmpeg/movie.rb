a3a41dbc0cddc9c3e45c945b5350a22eb69accba
David Backeus <duztdruid@gmail.com>
2010-04-06 14:25:13 +0200

    Correctly identify invalid movies on latet ffmpeg build.

--- lib/ffmpeg/movie.rb
+++ lib/ffmpeg/movie.rb
@@ -12,7 +12,7 @@ module FFMPEG
       stdin, stdout, stderr = Open3.popen3("ffmpeg -i '#{path}'") # Output will land in stderr
       output = stderr.read
       
-      @valid = output[/Unknown format/].nil?
+      @valid = output["Unknown format"].nil? && output["Invalid data found when processing input"].nil?
       
       output[/Duration: (\d{2}):(\d{2}):(\d{2}\.\d{1})/]
       @duration = ($1.to_i*60*60) + ($2.to_i*60) + $3.to_f
