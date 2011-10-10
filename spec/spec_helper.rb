3a6f122df15a51f53075cd8749f3688d35d34801
David Backeus <duztdruid@gmail.com>
2011-10-10 10:20:18 +0200

    Allow focus specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ require 'fileutils'
 FFMPEG.logger = Logger.new(nil)
 
 RSpec.configure do |config|
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 end
 
 def fixture_path
