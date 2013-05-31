b3cca13acdd48539351e0cfe1cba9b00995e7cb3
Jianbo Su <sujianbo@gmail.com>
2013-05-31 14:20:52 +0800

    change output encoding from ISO-8859-1 to UTF-8 while handling video with non English meta(say like Chinese

--- lib/ffmpeg/transcoder.rb
+++ lib/ffmpeg/transcoder.rb
@@ -121,7 +121,7 @@ module FFMPEG
     def fix_encoding(output)
       output[/test/]
     rescue ArgumentError
-      output.force_encoding("ISO-8859-1")
+      output.force_encoding("UTF-8")
     end
   end
 end
