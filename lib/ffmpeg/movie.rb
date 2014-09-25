5e6e1705611ae32424800e96535245532900afaf
Jonathan Santilli <jonathansantilli@gmail.com>
2014-09-25 19:27:30 +0200

    [INT-719] Make Movie duration attribute writable

--- lib/ffmpeg/movie.rb
+++ lib/ffmpeg/movie.rb
@@ -2,10 +2,12 @@ require 'time'
 
 module FFMPEG
   class Movie
-    attr_reader :path, :duration, :time, :bitrate, :rotation, :creation_time
+    attr_reader :path, :time, :bitrate, :rotation, :creation_time
     attr_reader :video_stream, :video_codec, :video_bitrate, :colorspace, :resolution, :sar, :dar
     attr_reader :audio_stream, :audio_codec, :audio_bitrate, :audio_sample_rate
     attr_reader :container
+
+    attr_accessor :duration
 
     def initialize(path)
       raise Errno::ENOENT, "the file '#{path}' does not exist" unless File.exists?(path)
