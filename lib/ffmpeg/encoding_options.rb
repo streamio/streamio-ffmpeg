b6f9823d723bcf027efb4bbafcb1a9fa7ab1805e
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2014-10-15 17:47:14 -0300

    crossfade

--- lib/ffmpeg/encoding_options.rb
+++ lib/ffmpeg/encoding_options.rb
@@ -170,7 +170,7 @@ module FFMPEG
     		filter << "," unless filter.empty? ;
     		filter << "[#{value[:idx1]}:v][#{value[:idx0]}:v]blend=all_expr='A*(if(gte(T,#{value[:time]}),1,T/#{value[:time]}))+B*(1-(if(gte(T,#{value[:time]}),1,T/#{value[:time]})))'"
     	end
-		"-filter_complex=\"#{filter}\"";
+		"-filter_complex \"#{filter}\"";
 	end
 	
     def convert_watermark_filter(value)
