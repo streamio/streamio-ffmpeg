69a188a683584dc2bdc8608c689814597e5e1fcd
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2014-10-15 18:55:36 -0300

    video rotation

--- lib/ffmpeg/encoding_options.rb
+++ lib/ffmpeg/encoding_options.rb
@@ -165,7 +165,7 @@ module FFMPEG
     end
 
     def convert_rotate(value)
-      "-vf \"rotate=#{(value*3.14)/180}\" -metadata rotate=#{@rotation+value}"
+      "-vf \"rotate=#{(value*3.14)/180}\" -metadata rotate=#{(@rotation.to_i+value)}"
     end
     
 	def convert_crossfade(values)
