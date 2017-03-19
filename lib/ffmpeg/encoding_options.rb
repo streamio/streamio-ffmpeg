932acbe923a4147688843a13eda7c9448b61a235
Mishaux <mishaux@gmail.com>
2017-03-18 19:34:29 -0600

    Add specs

--- lib/ffmpeg/encoding_options.rb
+++ lib/ffmpeg/encoding_options.rb
@@ -176,9 +176,9 @@ module FFMPEG
       ["-profile:v", value]
     end
 
-#    def convert_pix_fmt(value)
-#      ["-pix_fmt", value]
-#    end
+    def convert_pix_fmt(value)
+      ["-pix_fmt", value]
+    end
 
     def convert_watermark_filter(value)
       position = value[:position]
